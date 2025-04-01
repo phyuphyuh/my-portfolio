9fc223c4bf72d7eae6b6aa07904dbfe821efaf1a
Phyu Phyu Hlaing <phyuphyu.h27@gmail.com>
2025-04-01 21:09:16 +0630

    changed webdev paths

--- src/components/AnimatedHeading.jsx
+++ src/components/AnimatedHeading.jsx
@@ -1,47 +1,55 @@
-import { motion } from "motion/react";
+import { motion, AnimatePresence } from "motion/react";
 import { useEffect, useState } from "react";
 import styles from "./AnimatedHeading.module.scss";
 
 const AnimatedHeading = ({ letters, className }) => {
   const [key, setKey] = useState(0);
   const totalDuration = 0.4 + letters.length * 0.3;
+  const fadeOutDuration = totalDuration + 1.5;
 
   useEffect(() => {
     const interval = setInterval(() => {
       setKey((prevKey) => prevKey + 1);
-    }, totalDuration * 1000);
+    }, (totalDuration + fadeOutDuration) * 1000);
 
     return () => clearInterval(interval);
-  }, [totalDuration]);
+  }, [totalDuration, fadeOutDuration]);
 
   return (
-    <motion.div
-      key={key}
-      className={`${styles.animatedText} ${className}`}
-      initial="hidden"
-      animate="visible"
-    >
-      <svg xmlns="http://www.w3.org/2000/svg">
-        {letters.map((d, index) => (
-          <motion.path
-              key={index}
-              d={d}
-              fill="transparent"
-              stroke="var(--lighter-jet)"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              initial={{ strokeDasharray: "100%", strokeDashoffset: "100%" }}
-              animate={{ strokeDashoffset: 0 }}
-              transition={{
-                duration: 0.4,
-                ease: "easeInOut",
-                delay: 0.2 + index * 0.3,
-              }}
-          />
-        ))}
-      </svg>
-    </motion.div>
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={key}
+        className={`${styles.animatedText} ${className}`}
+        // initial="hidden"
+        // animate="visible"
+        initial={{ opacity: 1 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: fadeOutDuration, ease: "easeInOut" }}
+      >
+        <svg xmlns="http://www.w3.org/2000/svg">
+          {letters.map((d, index) => (
+            <motion.path
+                key={index}
+                d={d}
+                fill="transparent"
+                stroke="var(--lighter-jet)"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                initial={{ strokeDasharray: "100%", strokeDashoffset: "100%" }}
+                animate={{ strokeDashoffset: 0 }}
+                exit={{ strokeOpacity: 0 }}
+                transition={{
+                  duration: 0.4,
+                  ease: "easeInOut",
+                  delay: 0.3 + index * 0.3,
+                }}
+            />
+          ))}
+        </svg>
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
