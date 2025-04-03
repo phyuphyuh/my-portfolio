cb6149607bb3365491d9a8bd616060d30e6d2fb0
Phyu Phyu Hlaing <phyuphyu.h27@gmail.com>
2025-04-03 18:22:02 +0630

    contact tab w/ animation

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,3 +1,4 @@
+import { motion } from "motion/react";
 import styles from "./Contact.module.scss";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
@@ -6,22 +7,39 @@ import TabSVG from "../assets/contacttab3.svg";
 
 const Contact = () => {
   return (
-    <nav className={styles.contact}>
+    <motion.div
+      className={styles.contact}
+      initial={{ left: '-130px'}}
+      whileHover={{ left: '-83px'}}
+      transition={{ type: 'spring', stiffness: 300, damping: 20 }}
+    >
       <div className={styles.contactInner}>
         <img className={styles.tab} src={TabSVG} alt="Contact Tab" />
         <ul className={styles.contactList}>
-          <li className={styles.contactItem}>
+          <motion.li
+            className={styles.contactItem}
+            whileHover={{ scale: 1.2, rotate: 10 }}
+            transition={{ type: 'spring', stiffness: 300, damping: 10 }}
+          >
             <a href="https://github.com/phyuphyuh" target="_blank" className={styles.contactLink}><FontAwesomeIcon icon={faGithub} /></a>
-          </li>
-          <li className={styles.contactItem}>
+          </motion.li>
+          <motion.li
+            className={styles.contactItem}
+            whileHover={{ scale: 1.2, rotate: 10 }}
+            transition={{ type: 'spring', stiffness: 300, damping: 10 }}
+          >
             <a href="https://www.linkedin.com/in/phyuphyuhlaing" target="_blank" className={styles.contactLink}><FontAwesomeIcon icon={faLinkedin} /></a>
-          </li>
-          <li className={styles.contactItem}>
+          </motion.li>
+          <motion.li
+            className={styles.contactItem}
+            whileHover={{ scale: 1.2, rotate: 10 }}
+            transition={{ type: 'spring', stiffness: 300, damping: 10 }}
+          >
             <a href="mailto:phyuphyu.h27@gmail.com" className={styles.contactLink}><FontAwesomeIcon icon={faEnvelope} /></a>
-          </li>
+          </motion.li>
         </ul>
       </div>
-    </nav>
+    </motion.div>
   );
 };
 
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,4 +1,3 @@
-// import { motion } from "motion/react";
 import { useEffect, useState, useRef } from "react";
 import ScatterImages from './ScatterImages';
 import AnimatedHeading from './AnimatedHeading';
