ea7ce963e3595110ddc0f6dc2a836f04518ed2b6
Phyu Phyu Hlaing <phyuphyu.h27@gmail.com>
2025-09-07 20:51:24 +0630

    links

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,9 @@
 import { useState } from "react";
-import { motion, useScroll, useMotionValueEvent } from "motion/react";
+import { useScroll, useMotionValueEvent } from "motion/react";
 import styles from "./Contact.module.scss";
 import milo from "../assets/images/michan.png";
 import fifi from "../assets/images/muff.png";
 import chichi from "../assets/images/chichi.png";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const Contact = () => {
   const { scrollYProgress } = useScroll();
@@ -93,42 +90,6 @@ const Contact = () => {
         </div>
       </div>
     </div>
-    // <motion.div
-    //   className={styles.contact}
-    //   initial={false}
-    //   animate={{ left: isOpen ? '-84px' : '-130px' }}
-    //   whileHover={{ left: '-84px'}}
-    //   transition={{ type: 'spring', stiffness: 400, damping: 25 }}
-    //   onTap={() => setIsOpen((prev) => !prev)}
-    // >
-    //   <div className={styles.contactInner}>
-    //     {/* <img className={styles.tab} src={TabSVG} alt="Contact Tab" /> */}
-
-    //     <ul className={styles.contactList}>
-    //       <motion.li
-    //         className={styles.contactItem}
-    //         whileHover={{ scale: 1.2, rotate: 10 }}
-    //         transition={{ type: 'spring', stiffness: 300, damping: 10 }}
-    //       >
-    //         <a href="https://github.com/phyuphyuh" target="_blank"><FontAwesomeIcon icon={faGithub} /></a>
-    //       </motion.li>
-    //       <motion.li
-    //         className={styles.contactItem}
-    //         whileHover={{ scale: 1.2, rotate: 10 }}
-    //         transition={{ type: 'spring', stiffness: 300, damping: 10 }}
-    //       >
-    //         <a href="https://www.linkedin.com/in/phyuphyuhlaing" target="_blank"><FontAwesomeIcon icon={faLinkedin} /></a>
-    //       </motion.li>
-    //       <motion.li
-    //         className={styles.contactItem}
-    //         whileHover={{ scale: 1.2, rotate: 10 }}
-    //         transition={{ type: 'spring', stiffness: 300, damping: 10 }}
-    //       >
-    //         <a href="mailto:phyuphyu.h27@gmail.com"><FontAwesomeIcon icon={faEnvelope} /></a>
-    //       </motion.li>
-    //     </ul>
-    //   </div>
-    // </motion.div>
   );
 };
 
